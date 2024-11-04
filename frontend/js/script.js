b2b0c1bbb4443624c4be2de035ff421f0c39683a
joysqwz <joysqwzweb@gmail.com>
2024-11-04 14:22:29 +0300

    rel

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,125 +1,95 @@
-// const wrapper = document.querySelector('.wrapper');
-// const loginLink = document.querySelector('.login-link');
-// const registerLink = document.querySelector('.register-link');
+function clearRadioSelection(name) {
+	const radios = document.querySelectorAll(`input[name="${name}"]`)
+	radios.forEach(radio => {
+		radio.checked = false
+	})
+}
 
-// registerLink.addEventListener('click', ()=> {
-// 	wrapper.classList.add('active');
-// });
+const clearButtons = document.querySelectorAll('.content_skill-clear-button')
 
-// loginLink.addEventListener('click', ()=> {
-// 	wrapper.classList.remove('active');
-// });
+clearButtons.forEach(button => {
+	button.addEventListener('click', function () {
+		const groupNames = button.getAttribute('data-group').split(',')
+		groupNames.forEach(groupName => {
+			clearRadioSelection(groupName.trim())
+		})
+	})
+})
 
-(function() {
-	var canvas = document.createElement('canvas'),
-			ctx = canvas.getContext('2d'),
-			particles = [],
-			properties = {
-					bgColor: 'rgba(0, 0, 0, 0)', // Прозрачный фон
-					particleImages: [], // Массив для хранения изображений
-					particleRadius: 10, // Увеличиваем радиус для изображения
-					particleCount: 60,
-					particleMaxVelocity: 0.1,
-			};
+//
+const labels = document.querySelectorAll('.circle-container label')
 
-	// Устанавливаем размеры канваса
-	function setCanvasSize() {
-			canvas.width = innerWidth;
-			canvas.height = innerHeight;
+labels.forEach(label => {
+	label.addEventListener('dblclick', () => {
+		if (label.classList.contains('x')) {
+			label.classList.remove('x')
+			label.classList.add('o')
+		} else {
+			label.classList.remove('o')
+			label.classList.add('x')
+		}
+	})
+})
+
+const form = document.querySelector('.user-profile')
+const loginQuestion = document.querySelector('.login-question')
+const registerQuestion = document.querySelector('.register-question')
+
+registerQuestion.addEventListener('click', () => {
+	form.classList.add('active')
+})
+
+loginQuestion.addEventListener('click', () => {
+	form.classList.remove('active')
+})
+
+openBtn.addEventListener('click', () => {
+	modal.show()
+})
+
+document.addEventListener('click', (e) => {
+	const isClickOnOpenBtn = e.target === openBtn
+	if (isClickOnOpenBtn) return
+	const isClickOnDialog = e.target === modal
+	const isClickOnDialogContent = modal.contains(e.target)
+	const isClickOutsideOfDialog = !(isClickOnDialog || isClickOnDialogContent)
+	if (isClickOutsideOfDialog) {
+		modal.close()
+	}
+})
+
+let activeButton = document.getElementById('btn-one')
+let saveButton = null
+
+function changeMode(button) {
+	const activePage = document.querySelector('.content__page:not(.visually-hidden)')
+	if (activePage) {
+		activePage.classList.add('visually-hidden')
 	}
 
-	// Добавляем изображения в массив
-	for (let i = 1; i <= 19; i++) {
-			properties.particleImages.push(`./images/${i}.png`);
+	if (activeButton) {
+		activeButton.classList.remove('content__button--active')
+		activeButton.classList.add('content__button--inactive')
 	}
 
-	// Создаем массив объектов изображений
-	properties.particleImages = properties.particleImages.map(src => {
-			const img = new Image();
-			img.src = src;
-			return img;
-	});
+	activeButton = button
+	activeButton.classList.add('content__button--active')
+	activeButton.classList.remove('content__button--inactive')
 
-	// Устанавливаем стиль канваса
-	canvas.style.position = 'absolute';
-	canvas.style.top = '0';
-	canvas.style.left = '0';
-	canvas.style.zIndex = '0';
-
-	document.querySelector('body').appendChild(canvas);
-
-	// Убираем отступы и прокрутку
-	// document.body.style.margin = '0';
-	// document.body.style.overflow = 'hidden';
-
-	// Устанавливаем размеры канваса при загрузке
-	setCanvasSize();
-
-	window.onresize = function() {
-			setCanvasSize(); // Обновляем размеры канваса при изменении размера окна
+	const pageToShow = document.getElementById(button.dataset.target)
+	if (pageToShow) {
+		pageToShow.classList.remove('visually-hidden')
 	}
+}
 
-	class Particle {
-			constructor() {
-					this.x = Math.random() * canvas.width;
-					this.y = Math.random() * canvas.height;
-					this.velocityX = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
-					this.velocityY = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
-					this.image = properties.particleImages[Math.floor(Math.random() * properties.particleImages.length)]; // Случайное изображение
-			}
-			position() {
-					this.x + this.velocityX > canvas.width && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX < 0 ? this.velocityX *= -1 : this.velocityX;
-					this.y + this.velocityY > canvas.height && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY < 0 ? this.velocityY *= -1 : this.velocityY;
-					this.x += this.velocityX;
-					this.y += this.velocityY;
-			}
-			reDraw() {
-					ctx.drawImage(this.image, this.x - properties.particleRadius, this.y - properties.particleRadius, properties.particleRadius * 2, properties.particleRadius * 2);
-			}
-			reCalculateLife() {
-					// Удаляем логику жизни, если она не нужна
-			}
+function saveMode() {
+	if (activeButton) {
+		if (activeButton.classList.contains('content__button--save')) {
+			activeButton.classList.remove('content__button--save')
+			activeButton.classList.add('content__button--active')
+		} else {
+			activeButton.classList.remove('content__button--active')
+			activeButton.classList.add('content__button--save')
+		}
 	}
-
-	function reDrawBackground() {
-			ctx.fillStyle = properties.bgColor;
-			ctx.fillRect(0, 0, canvas.width, canvas.height);
-	}
-
-	function reDrawParticles() {
-			for (var i in particles) {
-					particles[i].reCalculateLife();
-					particles[i].position();
-					particles[i].reDraw();
-			}
-	}
-
-	function loop() {
-			ctx.clearRect(0, 0, canvas.width, canvas.height); // Очистка холста
-			reDrawBackground();
-			reDrawParticles();
-			requestAnimationFrame(loop);
-	}
-
-	function init() {
-			for (var i = 0; i < properties.particleCount; i++) {
-					particles.push(new Particle);
-			}
-			loop();
-	}
-
-	// Ждем, пока все изображения загрузятся, перед началом анимации
-	let imagesLoaded = 0;
-	properties.particleImages.forEach(img => {
-			img.onload = () => {
-					imagesLoaded++;
-					if (imagesLoaded === properties.particleImages.length) {
-							init();
-					}
-			};
-			img.onerror = () => {
-					console.error(`Ошибка загрузки изображения: ${img.src}`);
-			};
-	});
-
-}());
+}
